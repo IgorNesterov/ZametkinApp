b3ff61e668c084c2785f749bd81e9d9a68d3fa05
IgorN <nest_ig.of@mail.ru>
2020-05-04 15:00:11 +0300

    Добавьте файлы проекта.

---
+++ Zametkin/Zametkin/ListViewPage1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zametkin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage1 : ContentPage
+    {
+        //public ObservableCollection<string> Items { get; set; }
+
+        public string[] Notes { get; set; }
+
+        public ListViewPage1()
+        {
+
+            InitializeComponent();
+            Notes = new string[] { "Заметка 0", "Заметка 1"};
+            this.BindingContext = this;
+
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
---
+++ Zametkin/Zametkin/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace Zametkin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            FirebaseClient firebase = new FirebaseClient("https://zametkin-2c155.firebaseio.com/");
+            
+        }
+        private async void LogIn(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new ListViewPage1());
+        }
+    }
+
+}
